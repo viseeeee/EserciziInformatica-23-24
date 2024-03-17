0a3b9235cf4b8a8cd6a4a836aed4245730d54d5a
viseeeee <150442250+viseeeee@users.noreply.github.com>
2024-03-17 14:55:24 +0100

    contatore delle parole che iniziano con la lettera maiuscola con stampa di esse

--- stampaMaiuscole/src/stampaMaiuscole.java
+++ stampaMaiuscole/src/stampaMaiuscole.java
@@ -5,17 +5,26 @@ public class stampaMaiuscole {
         int contatore2 = 0;
         Scanner keyboard = new Scanner(System.in);
         int numeroP;
+
+        //stringa con tutte le lettere maiuscole
         String[] maiuscole = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
                 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+        //inserimento del numero delle parole
         System.out.println("Quante parole vuoi inserire?");
         numeroP = keyboard.nextInt();
+
+        //inserimento delle lunghezza dell'array
         String[] paroleM = new String[numeroP];
+        
         while (numeroP > 0) {
+            //inserimento della parola
             System.out.println("Inserisci la parola:");
             numeroP--;
             String parole = keyboard.next();
             char pCarattere = parole.charAt(0);
             for (int i = 0; i < maiuscole.length; i++) {
+                //conta le parole che iniziano con una lettera maiuscola
                 if (maiuscole[i].equals(Character.toString(pCarattere))) {
                     paroleM[contatore2] = parole;
                     contatore2++;
@@ -32,6 +41,7 @@ public class stampaMaiuscole {
         }
     }
 
+    //metodo che serve per stamapare le lettere maiuscole
     private static void ClrScr() {
         try {
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
